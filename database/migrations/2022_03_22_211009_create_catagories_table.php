e63f96d48e9c44dc706638c079c39b8716198121
Fahim Arko <arko.fahim@gmail.com>
2022-03-22 14:19:52 -0700

    created catagories table.

---
+++ database/migrations/2022_03_22_211009_create_catagories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catagories', function (Blueprint $table) {
+            $table->id();
+            $table->string('catagory_name');
+            $table->string('device_name');
+            $table->integer('serial_num');
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->integer('ram');
+            $table->integer('storage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catagories');
+    }
+};
