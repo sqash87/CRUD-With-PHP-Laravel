a1e810057e46b4b091f1d6863943803064618b05
Fahim Arko <arko.fahim@gmail.com>
2022-03-24 12:43:38 -0700

    creating two tables.

---
+++ database/migrations/2022_03_24_193850_create_equipments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('device_name');
+            $table->integer('cpu');
+            $table->integer('ram');
+            $table->integer('storage');
+            $table->string('price', $precision = 8, $scale = 2);
+            $table->integer('invoice');
+            $table->DateTime('purchaseDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+};
