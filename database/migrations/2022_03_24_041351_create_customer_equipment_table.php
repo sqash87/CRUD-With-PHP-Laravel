2da0d8870f1c916c56299bd713cd40aa1711b9f9
Fahim Arko <arko.fahim@gmail.com>
2022-03-23 21:32:56 -0700

    Update 2022_03_24_041351_create_customer_equipment_table.php

--- database/migrations/2022_03_24_041351_create_customer_equipment_table.php
+++ database/migrations/2022_03_24_041351_create_customer_equipment_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('csutomer_equipment');
+        Schema::dropIfExists('customer_equipment');
     }
 };
