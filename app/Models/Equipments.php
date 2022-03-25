2d4a18a37747ad83c6793384b3465bd4ddddf9a5
Fahim Arko <arko.fahim@gmail.com>
2022-03-24 23:28:43 -0700

    Update Equipments.php

--- app/Models/Equipments.php
+++ app/Models/Equipments.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Equipment extends Model
+class Equipments extends Model
 {
     protected $fillable = ['device_name', 'cpu', 'ram', 'storage', 'price', 'invoice','purchaseDate'];
     use HasFactory;
