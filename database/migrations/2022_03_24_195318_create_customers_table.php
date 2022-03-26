0d93f499ff9f44be613902f8b0135aae0108b875
Fahim Arko <arko.fahim@gmail.com>
2022-03-25 23:37:42 -0700

    Update 2022_03_24_195318_create_customers_table.php

--- database/migrations/2022_03_24_195318_create_customers_table.php
+++ database/migrations/2022_03_24_195318_create_customers_table.php
@@ -13,13 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->string('customer_name');
-            $table->string('customer_email');
-            $table->foreignId('equipment_id')->constrained('equipments');
-            $table->timestamps();
-        });
+        Schema::dropIfExists('customers');
     }
 
     /**
