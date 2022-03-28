4a3ba38e81ab8254e3c8363cf530085d60c3f26a
Fahim Arko <arko.fahim@gmail.com>
2022-03-28 03:21:46 -0700

    Update Customers.php

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Customers extends Model
 {
     use HasFactory;
-    protected $fillable = ['id','customer_name','customer_email','euipment_id','device_name'];
+    protected $fillable = ['id','customer_name','customer_email','equipment_id','device_name'];
 
     public function equipments()
     {
