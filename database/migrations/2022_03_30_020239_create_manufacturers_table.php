6ed2776b4478adeb71e26775be5c7c8c1a04b3f9
Fahim Arko <arko.fahim@gmail.com>
2022-03-29 19:12:35 -0700

    Creating tables.

---
+++ database/migrations/2022_03_30_020239_create_manufacturers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manufacturers', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name')->nullable();
+            $table->string('sales_email')->nullable();
+            $table->string('tech_email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manufacturers');
+    }
+};
---
+++ database/migrations/2022_03_30_020737_create_equipments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->nullable()->constrained('customers')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('manufacturer_id')->nullable()->constrained('manufacturers')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('equipment_name')->nullable();
+            $table->DateTime('purchaseDate')->nullable();
+            $table->string('cpu')->nullable();
+            $table->string('price', $precision = 8, $scale = 2)->nullable();
+            $table->string('ram')->nullable();
+            $table->string('storage')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+};
